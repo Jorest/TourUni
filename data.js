2ed29f69950699eccbe775dd6e2556d9fc172ece
jorest <jorest1994@gmail.com>
2022-01-21 19:36:55 +0100

    com1

---
+++ data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pan",
+      "name": "PAN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 326.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
